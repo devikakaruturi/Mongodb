dda53f979b97fb7f8b385692eae8c0d12ad6e093
dkarutur <dkarutur@IN-MUM-LDVM037.corp.capgemini.com>
2018-10-09 02:22:08 -0400

    first commit

---
+++ src/main/java/com/capgemini/productapp/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.capgemini.productapp.service;
+
+import java.util.List;
+
+import com.capgemini.productapp.entity.Product;
+import com.capgemini.productapp.exception.ProductNotFoundException;
+
+public interface ProductService {
+
+	public Product addProduct(Product product);
+
+	public Product updateProduct(Product product);
+
+	public Product findProductById(int productId) throws ProductNotFoundException;
+
+	public void deleteProduct(Product product);
+	
+	public List<Product> findProductByCategory(String category);
+}
+
+
